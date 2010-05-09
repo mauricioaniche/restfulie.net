2af2120ad5c05e6a60d66b29ca8d22b961358471
unknown <Administrador@.(none)>
2010-05-09 16:42:30 -0300

    fixing last commit

---
+++ Restfulie.Server.Tests/Transitions/TransitionsTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Restfulie.Server.Tests.Transitions
+{
+    [TestFixture]
+    public class TransitionsTests
+    {
+        [Test]
+        public void ShouldTransitToAControllerAction()
+        {
+            var transit = new Server.Transitions();
+            transit.Named("pay").Uses<SomeController>().SomeSimpleAction();
+
+            Assert.AreEqual("pay", transit.All.First().Name);
+        }
+
+    }
+}
