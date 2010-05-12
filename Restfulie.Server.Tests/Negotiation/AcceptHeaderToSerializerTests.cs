b07e891c53b4db8df0885941344aab2e49ea23be
Mauricio Aniche <mauricioaniche@gmail.com>
2010-05-11 22:08:19 -0300

    AcceptHeaderToSerializer ShouldReturnSerializerForAGivenMimeType

---
+++ Restfulie.Server.Tests/Negotiation/AcceptHeaderToSerializerTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Restfulie.Server.Marshalling.Serializers;
+using Restfulie.Server.Negotitation;
+
+namespace Restfulie.Server.Tests.Negotiation
+{
+    [TestFixture]
+    public class AcceptHeaderToSerializerTests
+    {
+        [Test]
+        public void ShouldReturnSerializerForAGivenMimeType()
+        {
+            var serializer = new AcceptHeaderToSerializer().For("application/xml");
+
+            Assert.IsTrue(serializer is XmlAndHypermediaSerializer);
+        }
+    }
+}
---
+++ Restfulie.Server/Negotitation/AcceptHeaderToSerializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Restfulie.Server.Marshalling.Serializers;
+
+namespace Restfulie.Server.Negotitation
+{
+    public class AcceptHeaderToSerializer
+    {
+        private static IDictionary<string, Type> types;
+        private static IDictionary<string, string> synonims;
+
+        static AcceptHeaderToSerializer()
+        {
+            types = new Dictionary<string, Type>
+                        {
+                            {"xml", typeof (XmlAndHypermediaSerializer)}
+                        };
+
+            synonims = new Dictionary<string, string>
+                           {
+                               {"application/xml", "xml"}
+                           };
+        }
+
+        public ISerializer For(string mimeType)
+        {
+            return (ISerializer)Activator.CreateInstance(types[synonims[mimeType]]);
+        }
+    }
+}
