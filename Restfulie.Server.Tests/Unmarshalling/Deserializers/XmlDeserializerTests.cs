d7e4b052ab2507506b971ac76a47fa9bf8748d6e
Mauricio Aniche <mauricioaniche@gmail.com>
2010-05-29 17:32:58 -0300

    XmlDeserializer ShouldDeserializeXML

---
+++ Restfulie.Server.Tests/Unmarshalling/Deserializers/XmlDeserializerTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Restfulie.Server.Tests.Fixtures;
+using Restfulie.Server.Unmarshalling.Deserializers;
+
+namespace Restfulie.Server.Tests.Unmarshalling.Deserializers
+{
+    [TestFixture]
+    public class XmlDeserializerTests
+    {
+        [Test]
+        public void ShouldDeserializeXml()
+        {
+            const string xml = "<SomeResource><Name>Some name</Name><Amount>100.00</Amount></SomeResource>";
+            var deserializer = new XmlDeserializer();
+            var resource = deserializer.Deserialize<SomeResource>(xml);
+
+            Assert.AreEqual("Some name", resource.Name);
+            Assert.AreEqual(100M, resource.Amount);
+        }
+    }
+}
---
+++ Restfulie.Server/Unmarshalling/Deserializers/XmlDeserializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Restfulie.Server.Unmarshalling.Deserializers
+{
+    public class XmlDeserializer : IResourceDeserializer
+    {
+        public T Deserialize<T>(string xml) where T : IBehaveAsResource
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            return (T)serializer.Deserialize(AStreamWith(xml));
+        }
+
+        private Stream AStreamWith(string xml)
+        {
+            var byteArray = new List<byte>();
+            foreach(var s in xml)
+            {
+                byteArray.Add(Convert.ToByte(s));
+            }
+
+            return new MemoryStream(byteArray.ToArray());
+        }
+    }
+}
