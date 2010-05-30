616253e9f5a2c83e5b930966a84e3762777a6074
Mauricio Aniche <mauricioaniche@gmail.com>
2010-05-30 12:11:17 -0300

    passing parameters in relations example

--- Web/Controllers/OrdersController.cs
+++ Web/Controllers/OrdersController.cs
@@ -14,21 +14,27 @@ namespace Web.Controllers
         {
             var orders = new List<IBehaveAsResource>()
                              {
-                                 new Order {Amount = 333.44, Date = DateTime.Now},
-                                 new Order {Amount = 555.66, Date = DateTime.Now}
+                                 new Order {Amount = 333.44, Date = DateTime.Now, Id = 111},
+                                 new Order {Amount = 555.66, Date = DateTime.Now, Id = 222}
                              };
 
             return new Success(orders);
         }
 
         [ActAsRestfulie]
-        public virtual ActionResult One()
+        public virtual ActionResult ReturnOne()
         {
-            return new Success(new Order {Amount = 333.44, Date = DateTime.Now});
+            return new Success(new Order {Amount = 333.44, Date = DateTime.Now, Id = 111});
+        }
+
+        [ActAsRestfulie]
+        public virtual ActionResult Pay(int id)
+        {
+            return new Success(new Order { Amount = 333.44, Date = DateTime.Now, Id = 111 });
         }
 
         [ActAsRestfulie(Name = "order", Type = typeof(Order))]
-        public virtual ActionResult DoSomething(Order order)
+        public virtual ActionResult ReceiveAResource(Order order)
         {
             return new Success(order);
         }
--- Web/Models/Order.cs
+++ Web/Models/Order.cs
@@ -9,10 +9,13 @@ namespace Web.Models
     {
         public DateTime Date { get; set; }
         public double Amount { get; set; }
+        public int Id { get; set; }
 
         public IList<Relation> GetRelations(Relations relations)
         {
-            relations.Named("pay").Uses<OrdersController>().Index();
+            relations.Named("all").Uses<OrdersController>().Index();
+            relations.Named("pay").Uses<OrdersController>().Pay(Id);
+            relations.Named("something").Uses<OrdersController>().ReceiveAResource(null);
 
             return relations.GetAll();
         }
