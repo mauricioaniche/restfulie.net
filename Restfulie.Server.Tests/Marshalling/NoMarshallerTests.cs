e72db946aa55b35853bbe8bbf665f4930daad49a
Mauricio Aniche <mauricioaniche@gmail.com>
2010-06-03 18:51:42 -0300

    two little tests for NoMarshaller

---
+++ Restfulie.Server.Tests/Marshalling/NoMarshallerTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Restfulie.Server.Marshalling;
+using Restfulie.Server.Tests.Fixtures;
+
+namespace Restfulie.Server.Tests.Marshalling
+{
+    [TestFixture]
+    public class NoMarshallerTests
+    {
+        [Test]
+        public void ShouldDoNothingWithAResource()
+        {
+            Assert.AreEqual(string.Empty, new NoMarshaller().Build(new SomeResource()));
+        }
+
+        [Test]
+        public void ShouldDoNothingWithAListOfResources()
+        {
+            Assert.AreEqual(string.Empty, new NoMarshaller().Build(new[] {new SomeResource()}));
+        }
+    }
+}
