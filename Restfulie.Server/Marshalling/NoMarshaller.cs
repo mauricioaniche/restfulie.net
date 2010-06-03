647cf5e2c9a1928e29f9f174c25f72231dc74a5c
Mauricio Aniche <mauricioaniche@gmail.com>
2010-06-03 17:57:16 -0300

    small comment in the code

--- Restfulie.Server/Marshalling/NoMarshaller.cs
+++ Restfulie.Server/Marshalling/NoMarshaller.cs
@@ -2,6 +2,7 @@
 
 namespace Restfulie.Server.Marshalling
 {
+    // used when html is the media type and we need to let asp.net mvc work!
     public class NoMarshaller : IResourceMarshaller
     {
         public string Build(IBehaveAsResource resource)
