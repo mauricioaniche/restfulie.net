4e7b62f1c658ca99175a31c4fc2d7f9092f10644
Mauricio Aniche <mauricioaniche@gmail.com>
2010-06-03 16:44:56 -0300

    content type decorator

---
+++ Restfulie.Server/Results/ContextDecorators/ContentTypeDecorator.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Restfulie.Server.Results.ContextDecorators
+{
+    public class ContentTypeDecorator : ContextDecorator
+    {
+        private readonly string contentType;
+
+        public ContentTypeDecorator(string contentType)
+        {
+            this.contentType = contentType;
+        }
+
+        public ContentTypeDecorator(string contentType, ContextDecorator nextDecorator) : base(nextDecorator)
+        {
+            this.contentType = contentType;
+        }
+
+        public override void Execute(ControllerContext context)
+        {
+            context.HttpContext.Response.ContentType = contentType;
+            Next(context);
+        }
+    }
+}
