dac98998cd3a43093e5a236ed951e2be8fcf0a24
Mauricio Aniche <mauricioaniche@gmail.com>
2010-06-03 18:23:17 -0300

    restfuliemarshaller tests

--- Restfulie.Server.Tests/Marshalling/RestfulieMarshallerTests.cs
+++ Restfulie.Server.Tests/Marshalling/RestfulieMarshallerTests.cs
@@ -1,107 +1,71 @@
-﻿//using System.Collections.Generic;
-//using System.IO;
-//using System.Web;
-//using System.Web.Mvc;
-//using Moq;
-//using NUnit.Framework;
-//using Restfulie.Server.Marshalling;
-//using Restfulie.Server.Marshalling.Serializers;
-//using Restfulie.Server.Marshalling.UrlGenerators;
-//using Restfulie.Server.Tests.Fixtures;
+﻿using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using Restfulie.Server.Marshalling;
+using Restfulie.Server.Marshalling.Serializers;
+using Restfulie.Server.Marshalling.UrlGenerators;
+using Restfulie.Server.Tests.Fixtures;
 
-//namespace Restfulie.Server.Tests.Marshalling
-//{
-//    [TestFixture]
-//    public class RestfulieMarshallerTests
-//    {
-//        private Mock<Relations> relations;
-//        private Mock<IResourceSerializer> serializer;
+namespace Restfulie.Server.Tests.Marshalling
+{
+    [TestFixture]
+    public class RestfulieMarshallerTests
+    {
+        private Mock<Relations> relations;
+        private Mock<IResourceSerializer> serializer;
 
-//        protected Mock<HttpResponseBase> response;
-//        protected Mock<HttpContextBase> http;
-//        protected Mock<ControllerContext> context;
-//        protected Mock<TextWriter> stream;
+        [SetUp]
+        public void SetUp()
+        {
+            relations = new Mock<Relations>(new Mock<IUrlGenerator>().Object);
+            serializer = new Mock<IResourceSerializer>();
+        }
 
-//        protected void SetUpRequest()
-//        {
-//            response = new Mock<HttpResponseBase>();
-//            http = new Mock<HttpContextBase>();
-//            context = new Mock<ControllerContext>();
+        [Test]
+        public void ShouldBuildResourceRepresentation()
+        {
+            var resource = new SomeResource();
 
-//            http.Setup(h => h.Response).Returns(response.Object);
-//            context.Setup(c => c.HttpContext).Returns(http.Object);
+            relations.Setup(t => t.GetAll()).Returns(SomeTransitions());
+            serializer.Setup(s => s.Serialize(resource, It.IsAny<IList<Relation>>())).Returns(SerializedResource());
 
-//            stream = new Mock<TextWriter>();
-//            response.Setup(p => p.Output).Returns(stream.Object);
-//        }
+            var builder = new RestfulieMarshaller(relations.Object, serializer.Object);
+            var representation = builder.Build(resource);
 
-//        [SetUp]
-//        public void SetUp()
-//        {
-//            relations = new Mock<Relations>(new Mock<IUrlGenerator>().Object);
-//            serializer = new Mock<IResourceSerializer>();
+            relations.VerifyAll();
+            serializer.VerifyAll();
+            Assert.AreEqual(representation, SerializedResource());
+        }
 
-//            SetUpRequest();
-//        }
+        [Test]
+        public void ShouldBuildListRepresentation()
+        {
+            var resources = new List<IBehaveAsResource> { new SomeResource(), new SomeResource() };
 
-//        [Test]
-//        public void ShouldBuildResourceRepresentation()
-//        {
-//            var resource = new SomeResource();
+            relations.Setup(t => t.GetAll()).Returns(SomeTransitions());
+            serializer.Setup(s => s.Serialize(It.IsAny<IDictionary<IBehaveAsResource, IList<Relation>>>())).Returns(SerializedListOfResources());
 
-//            relations.Setup(t => t.GetAll()).Returns(SomeTransitions());
-//            serializer.Setup(s => s.Serialize(resource, It.IsAny<IList<Relation>>())).Returns(SerializedResource());
+            var builder = new RestfulieMarshaller(relations.Object, serializer.Object);
+            var representation = builder.Build(resources);
 
-//            var builder = new RestfulieMarshaller(relations.Object, serializer.Object);
-//            builder.Build(context.Object, resource, new ResponseInfo());
-            
-//            relations.VerifyAll();
-//            serializer.VerifyAll();
-//            stream.Verify(s => s.Write(It.IsAny<string>()));
-//        }
+            relations.VerifyAll();
+            serializer.VerifyAll();
+            Assert.AreEqual(representation, SerializedListOfResources());
+        }
 
-//        [Test]
-//        public void ShouldBuildListRepresentation()
-//        {
-//            var resources = new List<IBehaveAsResource> {new SomeResource(), new SomeResource()};
+        private List<Relation> SomeTransitions()
+        {
+            return new List<Relation> { new Relation("pay", "Order", "Pay", new Dictionary<string, object>(), SerializedResource()) };
+        }
 
-//            relations.Setup(t => t.GetAll()).Returns(SomeTransitions());
-//            serializer.Setup(s => s.Serialize(It.IsAny<IDictionary<IBehaveAsResource, IList<Relation>>>())).Returns(SerializedResource());
-            
-//            var builder = new RestfulieMarshaller(relations.Object, serializer.Object);
-//            builder.Build(context.Object, resources, new ResponseInfo());
+        private static string SerializedResource()
+        {
+            return "resource here";
+        }
 
-//            relations.VerifyAll();
-//            serializer.VerifyAll();
-//            stream.Verify(s => s.Write(It.IsAny<string>()));
-//        }
-
-//        [Test]
-//        public void ShouldSetStatusCode()
-//        {
-//            var builder = new RestfulieMarshaller(relations.Object, serializer.Object);
-//            builder.Build(context.Object, new ResponseInfo {StatusCode = 123});
-
-//            response.VerifySet(c => c.StatusCode = 123);
-//        }
-
-//        [Test]
-//        public void ShouldSetLocation()
-//        {
-//            var builder = new RestfulieMarshaller(relations.Object, serializer.Object);
-//            builder.Build(context.Object, new ResponseInfo { Location = "/some/location" });
-
-//            response.VerifySet(c => c.RedirectLocation = "/some/location");
-//        }
-
-//        private List<Relation> SomeTransitions()
-//        {
-//            return new List<Relation> {new Relation("pay", "Order","Pay", new Dictionary<string, object>(), SerializedResource())};
-//        }
-
-//        private string SerializedResource()
-//        {
-//            return "resource here";
-//        }
-//    }
-//}
+        private static string SerializedListOfResources()
+        {
+            return "resource1 here, resource 2 here";
+        }
+    }
+}
