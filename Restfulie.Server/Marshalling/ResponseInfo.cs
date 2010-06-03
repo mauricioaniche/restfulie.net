784fe2aab03fdfb570caacdf9797092e98ccab35
Mauricio Aniche <mauricioaniche@gmail.com>
2010-06-02 21:34:12 -0300

    oops... removing useless properties in ResponseInfo

--- Restfulie.Server/Marshalling/ResponseInfo.cs
+++ Restfulie.Server/Marshalling/ResponseInfo.cs
@@ -1,12 +1,8 @@
-﻿using System.Web.Mvc;
-
-namespace Restfulie.Server.Marshalling
+﻿namespace Restfulie.Server.Marshalling
 {
     public class ResponseInfo
     {
         public string Location { get; set; }
         public int StatusCode { get; set; }
-        public ViewDataDictionary ViewData { get; set; }
-        public TempDataDictionary TempData { get; set; }
     }
 }
