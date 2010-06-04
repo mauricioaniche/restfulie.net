af28d5e30d9401d92222182f96a343ecfeba9d1c
Mauricio Aniche <mauricioaniche@gmail.com>
2010-06-04 15:00:12 -0300

    unmarshallerresolver initial implementation

---
+++ Restfulie.Server.Tests/Unmarshalling/UnmarshallerResolverTests.cs
@@ -0,0 +1,87 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Moq;
+using NUnit.Framework;
+using Restfulie.Server.Tests.Fixtures;
+using Restfulie.Server.Unmarshalling;
+
+namespace Restfulie.Server.Tests.Unmarshalling
+{
+    [TestFixture]
+    public class UnmarshallerResolverTests
+    {
+        private Mock<HttpContextBase> httpContext;
+        private Mock<HttpRequestBase> httpRequest;
+        private RequestContext requestContext;
+        private SomeController someController;
+        private ControllerContext controllerContext;
+        private Mock<ActionDescriptor> actionDescriptor;
+        private ActionExecutingContext context;
+
+        [SetUp]
+        public void CrazySetup()
+        {
+            httpContext = new Mock<HttpContextBase>();
+            httpRequest = new Mock<HttpRequestBase>();
+            requestContext = new RequestContext(httpContext.Object, new RouteData());
+
+            someController = new SomeController();
+            controllerContext = new ControllerContext(httpContext.Object, new RouteData(), someController);
+            actionDescriptor = new Mock<ActionDescriptor>();
+
+            httpContext.Setup(h => h.Request).Returns(httpRequest.Object);
+            context = new ActionExecutingContext(controllerContext, actionDescriptor.Object, new RouteValueDictionary()) { RequestContext = requestContext };
+   
+        }
+
+        [Test]
+        public void ItShouldUnmarshallOnlyWhenVerbIsAPostOrPut()
+        {
+            httpRequest.Setup(h => h.HttpMethod).Returns("GET");
+
+            var resolver = new UnmarshallerResolver();
+            resolver.DetectIn(context);
+
+            Assert.IsFalse(resolver.HasResource);
+        }
+
+        [Test]
+        public void ShouldDetectIfActionExpectsAResource()
+        {
+            httpRequest.Setup(h => h.HttpMethod).Returns("POST");
+
+            var resourceParameter = new Mock<ParameterDescriptor>();
+            resourceParameter.Setup(p => p.ParameterType).Returns(typeof(IBehaveAsResource));
+            var parameterList = new[] { resourceParameter.Object };
+            actionDescriptor.Setup(a => a.GetParameters()).Returns(parameterList);
+
+            var resolver = new UnmarshallerResolver();
+            resolver.DetectIn(context);
+
+            Assert.IsTrue(resolver.HasResource);
+        }
+
+        [Test]
+        public void ShouldFindTheResourceParameter()
+        {
+            httpRequest.Setup(h => h.HttpMethod).Returns("POST");
+
+            var nonResourceParameter = new Mock<ParameterDescriptor>();
+            var resourceParameter = new Mock<ParameterDescriptor>();
+
+            resourceParameter.Setup(p => p.ParameterType).Returns(typeof(SomeResource));
+            resourceParameter.Setup(p => p.ParameterName).Returns("resource");
+            nonResourceParameter.Setup(p => p.ParameterType).Returns(typeof (int));
+
+            var parameterList = new[] { nonResourceParameter.Object, resourceParameter.Object };
+            actionDescriptor.Setup(a => a.GetParameters()).Returns(parameterList);
+
+            var resolver = new UnmarshallerResolver();
+            resolver.DetectIn(context);
+
+            Assert.AreEqual(typeof(SomeResource), resolver.Type);
+            Assert.AreEqual("resource", resolver.ParameterName);
+        }
+    }
+}
---
+++ Restfulie.Server/Unmarshalling/UnmarshallerResolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Restfulie.Server.Unmarshalling
+{
+    public class UnmarshallerResolver
+    {
+        public bool HasResource { get; private set; }
+        public Type Type { get; private set; }
+        public string ParameterName { get; private set; }
+
+        public void DetectIn(ActionExecutingContext context)
+        {
+            if (!IsAValidHTTPMethod(context)) return;
+
+            foreach(var parameter in context.ActionDescriptor.GetParameters())
+            {
+                if(parameter.ParameterType == typeof(IBehaveAsResource) || parameter.ParameterType.GetInterface(typeof(IBehaveAsResource).FullName) != null)
+                {
+                    Type = parameter.ParameterType;
+                    ParameterName = parameter.ParameterName;
+                    HasResource = true;
+                    break;
+                }
+            }
+        }
+
+        private static bool IsAValidHTTPMethod(ControllerContext context)
+        {
+            return context.RequestContext.HttpContext.Request.HttpMethod.Equals("POST") || 
+                context.RequestContext.HttpContext.Request.HttpMethod.Equals("PUT");
+        }
+
+    }
+}
