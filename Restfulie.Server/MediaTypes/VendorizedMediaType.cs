78afdb702a1c19e781f8243ca1d8aaaa7b9f7a9b
Mauricio Aniche <mauricioaniche@gmail.com>
2010-06-09 22:01:49 -0300

    vendorized media type added

---
+++ Restfulie.Server/MediaTypes/VendorizedMediaType.cs
@@ -0,0 +1,42 @@
+﻿using Restfulie.Server.Marshalling;
+using Restfulie.Server.Marshalling.Serializers;
+using Restfulie.Server.Marshalling.UrlGenerators;
+using Restfulie.Server.Unmarshalling;
+using Restfulie.Server.Unmarshalling.Deserializers;
+
+namespace Restfulie.Server.MediaTypes
+{
+    public class VendorizedMediaType : IMediaType
+    {
+
+        public VendorizedMediaType(string format)
+        {
+            Synonyms = new[] {format};
+        }
+
+        public IResourceSerializer Serializer { get; set; }
+        public IHypermediaInserter Hypermedia { get; set; }
+        public IResourceDeserializer Deserializer { get; set; }
+
+        public string[] Synonyms
+        {
+            get; private set;
+        }
+
+        public IResourceMarshaller Marshaller
+        {
+            get
+            {
+                return
+                    new RestfulieMarshaller(new Relations(new AspNetMvcUrlGenerator()),
+                                            Serializer,
+                                            Hypermedia);
+            }
+        }
+
+        public IResourceUnmarshaller Unmarshaller
+        {
+            get { return new RestfulieUnmarshaller(Deserializer); }
+        }
+    }
+}
