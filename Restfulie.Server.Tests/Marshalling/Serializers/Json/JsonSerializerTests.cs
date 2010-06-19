e5e188beacf4fac129dccfc2b70347a502c6fc1e
unknown <fseixas@.(none)>
2010-06-18 22:53:48 -0300

    Added Json serializer implementation (without hypermedia)

---
+++ Restfulie.Server.Tests/Marshalling/Serializers/Json/JsonSerializerTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Restfulie.Server.Marshalling.Serializers;
+using Restfulie.Server.Marshalling.Serializers.Json;
+using Restfulie.Server.Tests.Fixtures;
+
+namespace Restfulie.Server.Tests.Marshalling.Serializers.Json
+{
+    [TestFixture]
+    public class JsonSerializerTests
+    {
+        private IResourceSerializer serializer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            serializer = new JsonSerializer();   
+        }
+
+        [Test]
+        public void ShouldSerializeAsResource()
+        {
+            var resource = new SomeResource {Amount = 123.45, Name = "John Doe"};
+            
+            var json = serializer.Serialize(resource);
+
+            Assert.That(json.Contains("\"Name\":\"John Doe\""));
+            Assert.That(json.Contains("\"Amount\":123.45"));
+        }
+        
+        [Test]
+        public void ShouldSerializeAListOfResources()
+        {
+            var resources = new []
+                                          {
+                                              new SomeResource {Amount = 123.45, Name = "John Doe"},
+                                              new SomeResource {Amount = 67.89, Name = "Sally Doe"}
+                                          };
+
+            var json = serializer.Serialize(resources);
+
+            Assert.That(json.Contains("\"Name\":\"John Doe\""));
+            Assert.That(json.Contains("\"Amount\":123.45"));
+            Assert.That(json.Contains("\"Name\":\"Sally Doe\""));
+            Assert.That(json.Contains("\"Amount\":67.89"));
+
+        }
+    }
+}
---
+++ Restfulie.Server/Marshalling/Serializers/Json/JsonSerializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Script.Serialization;
+
+namespace Restfulie.Server.Marshalling.Serializers.Json
+{
+    public class JsonSerializer : IResourceSerializer
+    {
+        public string Serialize(object resource)
+        {
+            var jsonSerializer = new JavaScriptSerializer();
+
+            return jsonSerializer.Serialize(resource);
+        }
+    }
+}
