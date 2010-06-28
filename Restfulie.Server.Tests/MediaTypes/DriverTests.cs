daea3f93877f407ad3b6d38d699b5b006fdba060
Mauricio Aniche <mauricioaniche@gmail.com>
2010-06-28 18:48:33 -0300

    simple test for Driver

---
+++ Restfulie.Server.Tests/MediaTypes/DriverTests.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using NUnit.Framework;
+using Restfulie.Server.Marshalling.Serializers;
+using Restfulie.Server.MediaTypes;
+using Restfulie.Server.Unmarshalling.Deserializers;
+
+namespace Restfulie.Server.Tests.MediaTypes
+{
+    [TestFixture]
+    public class DriverTests
+    {
+        [Test]
+        public void ShouldContainSerializerHypermediaInserterAndDeserializer()
+        {
+            var serializer = new Mock<IResourceSerializer>();
+            var hypermediaInserter = new Mock<IHypermediaInserter>();
+            var deserializer = new Mock<IResourceDeserializer>();
+
+            var driver = new Driver(serializer.Object, hypermediaInserter.Object, deserializer.Object);
+
+            Assert.AreSame(serializer.Object, driver.Serializer);
+            Assert.AreSame(hypermediaInserter.Object, driver.HypermediaInserter);
+            Assert.AreSame(deserializer.Object, driver.Deserializer);
+        }
+    }
+}
