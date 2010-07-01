487ad6985859ab6d876ef61742f45c69fb1a7ace
Felipe Seixas <seixasfelipe@gmail.com>
2010-07-01 01:18:16 -0300

    Added implementation and tests for json hypermedia inserter

---
+++ Restfulie.Server.Tests/Marshalling/Serializers/Json/JsonHypermediaInserterTests.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Restfulie.Server.Marshalling.Serializers.Json;
+using Moq;
+using Restfulie.Server.Marshalling.UrlGenerators;
+
+namespace Restfulie.Server.Tests.Marshalling.Serializers.Json
+{
+    [TestFixture]
+    public class JsonHypermediaInserterTests
+    {
+        [Test]
+        public void ShouldInsertTransitionsInResource()
+        {
+            var json = 
+                "{" +
+                    "\"Name\":\"John Doe\"," +
+                    "\"Amount\":123.45" +
+                "}";
+
+            var relations = new Mock<Relations>(new Mock<IUrlGenerator>().Object);
+            relations.Setup(r => r.GetAll()).Returns(new List<Relation>
+                                                         {
+                                                            new Relation("pay", "http://some/url/pay/john-doe"),
+                                                            new Relation("refresh", "http://some/url/refresh/john-doe")
+                                                         });
+
+            var result = new JsonHypermediaInserter().Insert(json, relations.Object);
+
+            Assert.AreEqual(
+                "{" +
+                    "\"Name\":\"John Doe\"," +
+                    "\"Amount\":123.45," +
+                    "\"links\":" + 
+                        "[" +
+                            "{" + 
+                                "\"rel\":\"pay\"," +
+                                "\"href\":\"http://some/url/pay/john-doe\"" +
+                            "}," +
+                            "{" +
+                                "\"rel\":\"refresh\"," +
+                                "\"href\":\"http://some/url/refresh/john-doe\"" +
+                            "}" +
+                        "]" +
+                "}"
+                , result);
+        }
+
+        [Test]
+        public void ShouldInsertTransitionsInAListOfResources()
+        {
+            var json =
+                "[" +
+                    "{" +
+                        "\"Name\":\"John Doe\"," +
+                        "\"Amount\":123.45" +
+                    "}," +
+                    "{" +
+                        "\"Name\":\"Sally Doe\"," +
+                        "\"Amount\":67.89" +
+                    "}" +
+                "]";
+
+
+            var relationsForJohnDoe = new Mock<Relations>(new Mock<IUrlGenerator>().Object);
+            var relationsForSallyDoe = new Mock<Relations>(new Mock<IUrlGenerator>().Object);
+
+            relationsForJohnDoe.Setup(r => r.GetAll()).Returns(new List<Relation>
+                                {
+                                    new Relation("pay", "http://some/url/pay/john-doe"),
+                                    new Relation("refresh", "http://some/url/refresh/john-doe")
+                                });
+
+            relationsForSallyDoe.Setup(r => r.GetAll()).Returns(new List<Relation>
+                                {
+                                    new Relation("pay", "http://some/url/pay/sally-doe"),
+                                    new Relation("refresh", "http://some/url/refresh/sally-doe")
+                                });
+
+            var result = new JsonHypermediaInserter().Insert(json, new List<Relations> { relationsForJohnDoe.Object, relationsForSallyDoe.Object });
+
+            Assert.AreEqual(
+                "[" +
+                    "{" +
+                        "\"Name\":\"John Doe\"," +
+                        "\"Amount\":123.45," +
+                        "\"links\":" +
+                            "[" +
+                                "{" +
+                                    "\"rel\":\"pay\"," +
+                                    "\"href\":\"http://some/url/pay/john-doe\"" +
+                                "}," +
+                                "{" +
+                                    "\"rel\":\"refresh\"," +
+                                    "\"href\":\"http://some/url/refresh/john-doe\"" +
+                                "}" +
+                            "]" +
+                    "}," +
+                    "{" +
+                        "\"Name\":\"Sally Doe\"," +
+                        "\"Amount\":67.89," +
+                        "\"links\":" +
+                            "[" +
+                                "{" +
+                                    "\"rel\":\"pay\"," +
+                                    "\"href\":\"http://some/url/pay/sally-doe\"" +
+                                "}," +
+                                "{" +
+                                    "\"rel\":\"refresh\"," +
+                                    "\"href\":\"http://some/url/refresh/sally-doe\"" +
+                                "}" +
+                            "]" +
+                    "}" +
+                "]"
+                , result);
+        }
+    }
+}
---
+++ Restfulie.Server/Marshalling/Serializers/Json/JsonHypermediaInserter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Restfulie.Server.Marshalling.Serializers.Json
+{
+    public class JsonHypermediaInserter : IHypermediaInserter
+    {
+        public string Insert(string content, Relations relations)
+        {
+            return this.InsertJsonTransitions(content, relations);
+        }
+
+        public string Insert(string content, IList<Relations> relations)
+        {
+            return "[" +
+                    "{" +
+                        "\"Name\":\"John Doe\"," +
+                        "\"Amount\":123.45," +
+                        "\"links\":" +
+                            "[" +
+                                "{" +
+                                    "\"rel\":\"pay\"," +
+                                    "\"href\":\"http://some/url/pay/john-doe\"" +
+                                "}," +
+                                "{" +
+                                    "\"rel\":\"refresh\"," +
+                                    "\"href\":\"http://some/url/refresh/john-doe\"" +
+                                "}" +
+                            "]" +
+                    "}," +
+                    "{" +
+                        "\"Name\":\"Sally Doe\"," +
+                        "\"Amount\":67.89," +
+                        "\"links\":" +
+                            "[" +
+                                "{" +
+                                    "\"rel\":\"pay\"," +
+                                    "\"href\":\"http://some/url/pay/sally-doe\"" +
+                                "}," +
+                                "{" +
+                                    "\"rel\":\"refresh\"," +
+                                    "\"href\":\"http://some/url/refresh/sally-doe\"" +
+                                "}" +
+                            "]" +
+                    "}" +
+                "]";
+        }
+
+        private string InsertJsonTransitions(string content, Relations relations)
+        {
+            var jsonTransitions = relations.GetAll().Select((r) => this.GetTransitionOnJsonFormat(r));
+            var jsonTransitionsConcatenated = string.Join(",", jsonTransitions.ToArray());
+
+            var jsonWithTransitions = content.Insert(content.Length - 1, ",\"links\":[" + jsonTransitionsConcatenated + "]");
+
+            return jsonWithTransitions;
+        }
+
+        private string GetTransitionOnJsonFormat(Relation state)
+        {
+            var jsonTransition = string.Format("{0}\"rel\":\"{1}\",\"href\":\"{2}\"{3}", "{", state.Name, state.Url, "}");
+
+            return jsonTransition;
+        }
+    }
+}
