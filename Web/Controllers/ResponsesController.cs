53ba1c39b854b0f11f826307b4f1c42aaf5b864e
Mauricio Aniche <mauricioaniche@gmail.com>
2010-07-07 00:08:01 -0300

    ResponseController created for system tests

---
+++ Web/Controllers/ResponsesController.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+using Restfulie.Server;
+using Restfulie.Server.Results;
+
+namespace Web.Controllers
+{
+    [ActAsRestfulie]
+    public class ResponsesController : Controller
+    {
+        public virtual ActionResult Success()
+        {
+            return new Success();
+        }
+
+        public virtual ActionResult BadRequest()
+        {
+            return new BadRequest();
+        }
+
+        public virtual ActionResult Created()
+        {
+            return new Created();
+        }
+
+        public virtual ActionResult InternalServerError()
+        {
+            return new InternalServerError();
+        }
+
+        public virtual ActionResult NotFound()
+        {
+            return new NotFound();
+        }
+
+        public virtual ActionResult PreconditionFailed()
+        {
+            return new PreconditionFailed();
+        }
+
+        public virtual ActionResult SeeOther()
+        {
+            return new SeeOther(Url.Action("Success"));
+        }
+    }
+}
