4b9ade19b729dd9a7426aa8b1eefb64d7df6f234
Mauricio Aniche <mauricioaniche@gmail.com>
2010-07-10 18:36:26 -0300

    setting correct version at assembly info

--- Restfulie.Server/Properties/AssemblyInfo.cs
+++ Restfulie.Server/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Restfulie.Server")]
+[assembly: AssemblyTitle("Restfulie.NET")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Restfulie.Server")]
+[assembly: AssemblyCompany("Created by Mauricio Aniche")]
+[assembly: AssemblyProduct("Restfulie.NET")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
