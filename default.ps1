3e079ede3c9e289a0bc5ceca6287c1dcea2587af
Pedro Reys <pedroreys@gmail.com>
2012-07-25 23:37:31 -0500

    adds output to build script to show package version

--- default.ps1
+++ default.ps1
@@ -91,6 +91,7 @@ using System.Runtime.InteropServices;
 
 function global:create-nuspec($package_version)
 {
+  Write-Host "Generating nuspec for version: $package_version"
     "<?xml version=""1.0""?>
 <package xmlns=""http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd"">
   <metadata>
