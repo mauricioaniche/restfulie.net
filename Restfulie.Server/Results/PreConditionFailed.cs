abb742039f452e13065190ee11dc858851dff854
Leon Maia <leon@leonmaia.com>
2013-06-13 18:00:48 -0300

    PreConditionFailed now can receives a model

--- Restfulie.Server/Results/PreConditionFailed.cs
+++ Restfulie.Server/Results/PreConditionFailed.cs
@@ -1,13 +1,21 @@
-﻿using System.Net;
+﻿using System.Linq;
+using System.Net;
 using Restfulie.Server.Results.Decorators;
 
 namespace Restfulie.Server.Results
 {
     public class PreconditionFailed : RestfulieResult
     {
+        public PreconditionFailed() {}
+        
+        public PreconditionFailed(object model) 
+            : base(model) {}
+        
         public override ResultDecorator GetDecorators()
         {
-            return new StatusCode((int) HttpStatusCode.PreconditionFailed);
+            return new StatusCode((int)HttpStatusCode.PreconditionFailed,
+                                  new ContentType(MediaType.Synonyms.First(),
+                                                  new Content(BuildContent())));
         }
     }
 }
